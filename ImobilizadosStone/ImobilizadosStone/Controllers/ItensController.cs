692c2f0008b30b9bd1c9245b97e97737f47f9c0f
Eduardo Norio Kuwakino <kuwakino@gmail.com>
2017-10-11 11:00:08 -0300

    Criação de solution; Inclusão de projeto Web API;

---
+++ ImobilizadosStone/ImobilizadosStone/Controllers/ItensController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/itens")]
+    public class ItensController : Controller
+    {
+        // GET api/itens
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/itens?status={status}
+        [HttpGet]
+        public IEnumerable<string> Get([FromQuery] string status)
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/itens/{id}
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/itens
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/itens/{id}
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/itens/{id}
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
